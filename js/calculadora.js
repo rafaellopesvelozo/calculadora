2be4ed430df09bdf9760beff933f800c12847a2e
Rafael lopes <rafaellopesvelozo@gmail.com>
2022-05-12 19:28:32 -0300

    primerira versao da cauculadora.html, calculadora.js e calculadora.css

---
+++ js/calculadora.js
@@ -0,0 +1,26 @@
+function insert(num){
+  var numero = document.getElementById('result').innerHTML;
+  document.getElementById('result').innerHTML = numero + num;
+ 
+}
+
+function clean(){
+  document.getElementById('result').innerHTML = ""
+}
+
+function back(){
+ var resultado = document.getElementById('result').innerHTML;
+
+ document.getElementById('result').innerHTML = resultado.substring(0, resultado.length -1); // comando para apagar numero de 1 em 1 
+}
+
+function calcular(){
+  var resultado = document.getElementById('result').innerHTML;
+  if(resultado){
+    document.getElementById('result').innerHTML = eval (resultado);
+    //"eval" comando q faz os calculos e
+  }
+  else{
+    document.getElementById('result').innerHTML = "Nada para calcular"
+  }
+}
